bb5dcca54d6acd17b837e1f6aaaf25906211dd3b
programminglk <programmingslk@gmail.com>
2023-05-21 23:40:11 +0530

    initial commit

---
+++ src/app/locales/app_values.js
@@ -0,0 +1,30 @@
+export default {
+    game_list: ["Thesis Elements", "Game 2", "Game 3", "Game 4"],
+
+    thesis_elements: [
+        'Output',
+        'Input',
+        'Hypothesis',
+        'Users',
+        'Future work',
+        'Features',
+        'Variables',
+        'Codes',
+        'Solution',
+        'Citation',
+        'Problem',
+        'Curiosity',
+        'Conclusion',
+        'Technology',
+        'Motivation',
+        'Limitations',
+        'Objectives',
+        'References',
+        'Software',
+        'Hardware',
+        'Results',
+        'Key Researches',
+        'Process'
+    ],
+}
+
